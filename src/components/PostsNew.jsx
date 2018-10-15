b1b37b9d7303f068abf3968b032c90d977610ea7
adam <adam@couleetechlink.com>
2018-10-15 13:05:17 -0500

    started validation

--- src/components/PostsNew.jsx
+++ src/components/PostsNew.jsx
@@ -1,31 +1,58 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-  renderTitleField(field){
-    return(
-      <div>
-        <input
-          type="text"
-          {...field.input}
-        />
+  renderField(field) {
+    console.log(field.meta.error);
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
-    )
+    );
   }
   render() {
     return (
       <div>
-          <form>
-            <Field
-                name="title"
-                component={this.renderTitleField}
-            />
-          </form>
+        <form>
+          <Field label="Title" name="title" component={this.renderField} />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
-    )
+    );
   }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Please Enter an Title";
+  }
+  if (!values.categories) {
+    errors.categories = "Please Enter an Category";
+  }
+  if (!values.content) {
+    errors.content = "Please Enter some Content";
+  }
+  // empty errors means it has passed validation
+  return errors;
 }
 // form `namespaces`
 export default reduxForm({
-    form: 'PostNewForm'
+  validate,
+  form: "PostNewForm"
 })(PostsNew);
