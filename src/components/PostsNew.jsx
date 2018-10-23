a59e04fca34f17db2a8eb4a7259be1b5c2316f35
Adam Harpur <adam@harpur.io>
2018-10-23 13:26:33 -0500

    added posts show

--- src/components/PostsNew.jsx
+++ src/components/PostsNew.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import reducers from './reducers';
 import PostsIndex from './components/PostsIndex';
 import PostsNew from './components/PostsNew';
+import PostsShow from './components/PostsShow';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,8 +17,9 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <Switch>
-        <Route path="/post/new" component={PostsNew}></Route>
-        <Route path="/" component={PostsIndex}></Route>
+          <Route path="/posts/new" component={PostsNew}></Route>
+          <Route path="/posts/:id" component={PostsShow}></Route>
+          <Route path="/" component={PostsIndex}></Route>
         </Switch>
       </div>
     </BrowserRouter>
