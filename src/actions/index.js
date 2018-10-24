bf55f72902869b4c4d26a38740cd2ba7ae2b37a0
Adam Harpur <adam@harpur.io>
2018-10-24 17:53:58 -0500

    rendering individual posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=HARPS116'
@@ -20,6 +22,14 @@ export function createPost(values, callback){
     const req = axios.post(`${ROOT_URL}/posts${API_KEY}`, values).then(() => { callback() } )
     return {
         type: CREATE_POST,
+        payload: req
+    }
+}
+
+export function fetchPost(id){
+    const req = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+    return {
+        type: FETCH_POST,
         payload: req
     }
 }
--- src/components/PostsShow.jsx
+++ src/components/PostsShow.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
+
 
 class PostsShow extends Component {
-    render() {
-        return (
-           <div>Posts Show!</div>
-        );
+    
+    componentDidMount(){
+        const {id} = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+    // render() {
+    //     const { post } = this.props;
+    //     return (
+    //        <div>
+    //            <h3>{post.title}</h3>
+    //             <h6>Categories: {post.catergories}</h6>
+    //             <p>{post.content}</p>
+    //        </div>
+    //     );
+    // }
+    render(){
+        const { post } = this.props;
+        if(post){
+            return(
+                <div>
+                  {post.title}
+                </div>
+            )
+        }else{
+            return null
+        }
     }
 }
 
-export default PostsShow;
+function mapStateToProps({posts}, ownProps){
+    const state = { post: posts[ownProps.match.params.id] }
+    return state;
+}
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
